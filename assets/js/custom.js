2cb6b65161d714b372ae6c232ea59bde2f8ba7c8
Yuri Panshin <ypanshin@gmail.com>
2021-02-15 20:57:30 -0500

    add experiance

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -114,3 +114,18 @@ $(window).load(function () {
     });
 
 });
+
+
+// background color
+$('[data-color]').each(function () {
+    $(this).css({
+        'background-color': $(this).data('color')
+    });
+});
+
+// progress bar
+$('[data-progress]').each(function () {
+    $(this).css({
+        'bottom': $(this).data('progress')
+    });
+});
